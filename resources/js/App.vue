7c1994d4e6346d5b7bfaa90ba6199efb77679dd0
SaiWannaAung <saiwannaaung095@gmail.com>
2020-02-17 12:47:00 +0630

    Vue Testing

--- resources/js/App.vue
+++ resources/js/App.vue
@@ -1,5 +1,9 @@
 <template>
-<!--    <router-link to="/testhome">About</router-link>-->
-<!--    <router-link to="/testhome">Home</router-link>-->
-    <router-view></router-view>
+    <div>
+        <router-link to="/testhome">Home</router-link>
+            <router-link to="/testabout">About</router-link>
+
+        <router-view></router-view>
+    </div>
+
 </template>
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -14,7 +14,8 @@
 <nav class="navbar navbar-expand-sm bg-light">
     <ul class="navbar-nav">
         <li class="nav-item">
-            <a class="nav-link" href="#">Link 1</a>
+
+            <a class="nav-link" href="/testhome">Link 1</a>
         </li>
         <li class="nav-item">
             <a class="nav-link" href="#">Link 2</a>
